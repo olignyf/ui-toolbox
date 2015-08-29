9b4cc6d799c42a245367afec98cb082167cf66a0
Francois Oligny-Lemieux <flemieux@haivision.com>
2015-08-29 15:29:35 -0400

    * added ajax  * some improvements

--- ui-toolbox.js
+++ ui-toolbox.js
@@ -5,10 +5,14 @@
 //  Description : Reusable DOM manipulation functions.
 //   Written by : Francois Oligny-Lemieux
 // Date Created : 02.Nov.2014
+//     Modified : 22.Nov.2014 (added jsonp request, added [data-template])
 // 
 // Browser support : 
-//  DOM manipulations: IE6+, Firefox 1.5+, Chrome, Safari, 
+//  DOM manipulations: IE8+, Firefox 1.5+, Chrome, Safari
 //  HTML5: IE9+, Firefox 22+, Chrome 15.
+//
+// TODO: Write fallback to querySelector for IE6 & IE7
+//
 //////////////////////////////////////////////////////
 "use strict";
 define(function (require) {
@@ -126,7 +130,13 @@ define(function (require) {
 	{
 		// IE8 does not support querySelectorAll with spaces, i.e. hierarchical selections
 		if (parent != null)
-		{	return parent.querySelectorAll(selector);
+		{
+			if (typeof parent.querySelectorAll == "undefined")
+			{
+				if (console) console.log("parent.querSelectorAll is invalid for selector:" + selector);
+				if (console) console.trace();
+			}
+			return parent.querySelectorAll(selector);
 		}
 		return document.querySelectorAll(selector);
 	};
@@ -345,48 +355,41 @@ define(function (require) {
 		return html;
 	};
 
-	// TODO: to make this more reusable, change to a recursive function 
-	Toolbox.ReplacePlain = function(html, model)
+	// Replace model in HTML string
+	// Targets on %%variable-name%%
+	// Suports array %%cities[0].temperature%%
+	Toolbox.ReplacePlain = function(html, model, base)
 	{
 		var regex;
+		if (base == null) base = "";
 		
 		for (var item in model)
 		{
-			if (typeof model[item] == "object")
+			if (typeof model[item] == "function")
+			{	// skip functions
+			}
+			else if (Object.prototype.toString.call( model[item] ) === '[object Array]')
 			{
-				for (var level2 in model[item])
+				for (var iterator in model[item])
 				{
-					if (Object.prototype.toString.call( model[item] ) === '[object Array]')
-					{
-						if (typeof model[item][level2] == "object")
-						{
-							for (var level3 in model[item][level2])
-							{
-								regex = new RegExp("%%"+item+"\\["+level2+"\\]\\."+level3+"%%", "g"); // the "g" is important to replace all
-								html = html.replace(regex, model[item][level2][level3]);
-							}
-						}
-						else
-						{
-							regex = new RegExp("%%"+item+"\\["+level2+"\\]%%", "g"); // the "g" is important to replace all
-							html = html.replace(regex, model[item][level2]);
-						}
-					}
-					else
-					{	regex = new RegExp("%%"+item+"\\."+level2+"%%", "g"); // the "g" is important to replace all
-						html = html.replace(regex, model[item][level2]);
-					}
+					html = Toolbox.ReplacePlain(html, model[item][iterator], base+item+"\\["+iterator+"\\]\\.");
 				}
+			}
+			else if (typeof model[item] == "object")
+			{
+				html = Toolbox.ReplacePlain(html, model[item], base+item+"\\.");
 			}
 			else
 			{
-				regex = new RegExp("%%"+item+"%%", "g"); // the "g" is important to replace all
+				regex = new RegExp("%%"+base+item+"%%", "g"); // the "g" is important to replace all
 				html = html.replace(regex, model[item]);
 			}
 		}
 		return html;
 	};
 	
+	// Replaces using innerHTML on existing DOM elements
+	// Targets on attributes data-target="model-variable-name" and data-template="templateName"
 	Toolbox.ReplaceInPlace = function(rootEl, model, base)
 	{
 		var regex;
@@ -394,19 +397,19 @@ define(function (require) {
 		
 		for (var item in model)
 		{
-			if (Object.prototype.toString.call( model[item] ) === '[object Array]')
+			if (typeof model[item] == "function")
+			{	// skip functions
+			}
+			else if (Object.prototype.toString.call( model[item] ) === '[object Array]')
 			{
 				for (var iterator in model[item])
 				{
-					Toolbox.ReplaceInPlace(rootEl, model[item][iterator], base+item+"["+iterator+"].");
+					Toolbox.ReplaceInPlace(rootEl, model[item][iterator], base+item+"\\["+iterator+"\\]\\.");
 				}
 			}
 			else if (typeof model[item] == "object")
 			{
-				//for (var level2 in model[item])
-				//{
-					Toolbox.ReplaceInPlace(rootEl, model[item], base+item+".");
-				//}
+				Toolbox.ReplaceInPlace(rootEl, model[item], base+item+"\\.");
 			}
 			else
 			{
@@ -604,8 +607,58 @@ define(function (require) {
 		{
 			if (console) console.log("[xmlhttp] Error send(): " + e);
 		}		
-	};
-
+	};	
+	
+	/* sandbox
+			var mingleSingle = function(key, value, model)
+			{
+				if (key.length > 0 && key.indexOf(".") > 0)
+				{
+					//console.log("mingling "+key);
+					var parts = key.split(".");
+					if (parts == null || parts.length <= 1)
+						return;
+					var subset = (typeof model.get == "function") ? model.get([parts[0]]) : model[parts[0]];
+					if (parts.length === 2)
+					{
+						subset[parts[1]] = value;
+					}
+					else if (parts.length > 2)
+					{
+						var newKey = key.substring(parts[0].length+1);
+						mingleSingle(newKey, value, subset);
+					}
+				}
+			};
+			// mingle settings
+			var mingle = function(settings, model)
+			{
+				for (var key in settings)
+				{
+					if (key.length > 0 && key.indexOf(".") > 0)
+					{
+						//console.log("mingling "+key);
+						var parts = key.split(".");
+						if (parts.length === 2)
+						{
+							var subset = model.get([parts[0]]);
+							subset[parts[1]] = settings[key];
+						}
+						else if (parts.length > 2)
+						{
+							var subset = model.get([parts[0]]);
+							var newKey = key.substring(parts[0].length+1);
+							mingleSingle(newKey, settings[key], subset);
+						}
+					}
+				}
+			};
+			self.settings["config.test.c"] = "";
+			self.settings["config.test.third.smack"] = "";
+			mingle(self.settings, this.model);
+	*/
+			
+	
 	return Toolbox;
 });
 
